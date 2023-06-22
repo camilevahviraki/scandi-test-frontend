6a5edcc2abd013d896335d0a7452229f7156b263
camilevahviraki <camilevahviraki@gmail.com>
2023-06-22 02:43:15 +0200

    Add packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Products from "./components/productsPage/Products";
+import AddProduct from "./components/addProduct/addProduct";
+import Footer from "./components/footer/Footer";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/add_product" element={<AddProduct />} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
