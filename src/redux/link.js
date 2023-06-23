7235f4dc27c556594c05afe36e377ccac217441a
camilevahviraki <camilevahviraki@gmail.com>
2023-06-23 16:13:46 +0200

    Update link.js

--- src/redux/link.js
+++ src/redux/link.js
@@ -1,4 +1,5 @@
 // const linkURL = 'http://localhost:8000';
 const linkURL= 'https://juniortest-camile-vahviraki.000webhostapp.com';
+// const linkURL='http://junior-test-camile-vahviraki.free.nf';
 
 export default linkURL;
